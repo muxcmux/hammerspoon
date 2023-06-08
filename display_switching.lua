1533eef13d19ac8f7889e8f963ad66eb180d9f27
muxcmux <storm.bg@gmail.com>
2023-06-08 10:28:38 +0100

    change display switching key combo to make it harder to hit by accident

--- display_switching.lua
+++ display_switching.lua
@@ -1,7 +1,7 @@
-hs.hotkey.bind({"ctrl", "shift", "alt"}, "F", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "escape", function()
   hs.execute("/Users/muxcmux/src/m1ddc/m1ddc display 1 set input 25")
 end)
 
-hs.hotkey.bind({"ctrl", "shift", "alt"}, "G", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "delete", function()
   hs.execute("/Users/muxcmux/src/m1ddc/m1ddc display 1 set input 18")
 end)
