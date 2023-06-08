dfbf84f0a2a698f96c8331f003535240faaa9ea0
muxcmux <storm.bg@gmail.com>
2023-06-08 10:11:59 +0100

    refactor window management

--- window_management.lua
+++ window_management.lua
@@ -5,6 +5,8 @@ local function toggle_left()
   local window = win:frame()
   local screen = win:screen():frame()
 
+  local width = 2/3
+
   if window.x + window.w >= screen.w and window.w < screen.w then
     win:move({
       x = screen.x,
@@ -13,27 +15,17 @@ local function toggle_left()
       h = window.h,
     }, nil, true, ANIM_SPEED)
   elseif window.w < math.floor(screen.w * 1/3) or window.w >= math.ceil(screen.w * 2/3) then
-    win:move({
-      x = screen.x,
-      y = screen.y,
-      w = math.floor(screen.w * 1/3),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
+    width = 1/3
   elseif window.w < math.floor(screen.w * 1/2) then
-    win:move({
-      x = screen.x,
-      y = screen.y,
-      w = math.floor(screen.w * 1/2),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
-  else
-    win:move({
-      x = screen.x,
-      y = screen.y,
-      w = math.ceil(screen.w * 2/3),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
+    width = 1/2
   end
+
+  win:move({
+    x = screen.x,
+    y = screen.y,
+    w = width == 2/3 and math.ceil(screen.w * width) or math.floor(screen.w * width),
+    h = screen.h,
+  }, nil, true, ANIM_SPEED)
 end
 
 local function toggle_right()
@@ -41,7 +33,11 @@ local function toggle_right()
   local window = win:frame()
   local screen = win:screen():frame()
 
-  if window.x == screen.x and window.w < screen.w then
+  local x = 2/3
+  local width = 1/3
+
+  if window.x == screen.x
+     and window.w < screen.w then
     win:move({
       x = screen.w - window.w,
       y = window.y,
@@ -49,27 +45,19 @@ local function toggle_right()
       h = window.h,
     }, nil, true, ANIM_SPEED)
   elseif window.w > math.ceil(screen.w * 2/3) or window.w <= math.floor(screen.w * 1/3) then
-    win:move({
-      x = math.floor(screen.w * 1/3),
-      y = screen.y,
-      w = math.ceil(screen.w * 2/3),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
+    x = 1/3
+    width = 2/3
   elseif window.w > math.floor(screen.w * 1/2) then
-    win:move({
-      x = math.floor(screen.w * 1/2),
-      y = screen.y,
-      w = math.floor(screen.w * 1/2),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
-  else
-    win:move({
-      x = math.ceil(screen.w * 2/3),
-      y = screen.y,
-      w = math.floor(screen.w * 1/3),
-      h = screen.h,
-    }, nil, true, ANIM_SPEED)
+    x = 1/2
+    width = 1/2
   end
+
+  win:move({
+    x = x == 2/3 and math.ceil(screen.w * x) or math.floor(screen.w * x),
+    y = screen.y,
+    w = width == 2/3 and math.ceil(screen.w * width) or math.floor(screen.w * width),
+    h = screen.h,
+  }, nil, true, ANIM_SPEED)
 end
 
 local function toggle_center()
