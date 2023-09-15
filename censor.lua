04e2c907b14fd9b036ff0864c76082b3f2f34e20
muxcmux <storm.bg@gmail.com>
2023-09-15 17:26:35 +0100

    move device sourcing inside the functions to prevent screw-ups when reconnecting devices

--- censor.lua
+++ censor.lua
@@ -1,17 +1,17 @@
-local bleep = hs.sound.getByFile("/Users/muxcmux/.hammerspoon/bleep.mp3")
-
-bleep:volume(0.1)
-
-local mic = hs.audiodevice.findDeviceByName("Q9-1")
-
-bleep:device("BlackHole2ch_UID")
-
 local function censor_start()
+  local bleep = hs.sound.getByFile("/Users/muxcmux/.hammerspoon/bleep.mp3")
+  bleep:volume(0.1)
+  local mic = hs.audiodevice.findDeviceByName("Q9-1")
+  bleep:device("BlackHole2ch_UID")
   mic:setInputMuted(true)
   bleep:play()
 end
 
 local function censor_end()
+  local bleep = hs.sound.getByFile("/Users/muxcmux/.hammerspoon/bleep.mp3")
+  bleep:volume(0.1)
+  local mic = hs.audiodevice.findDeviceByName("Q9-1")
+  bleep:device("BlackHole2ch_UID")
   bleep:stop()
   mic:setInputMuted(false)
 end
